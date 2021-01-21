6282caa020fb3f73e40b9b0a83290c48b01ccff5
RealEmur <68711637+RealEmur@users.noreply.github.com>
2021-01-21 19:55:31 +0300

    A little fix for turkish characters

--- admin/veritabani/baglan.php
+++ admin/veritabani/baglan.php
@@ -1,7 +1,9 @@
 <?php 
 try{
 	$db = new PDO("mysql:host=localhost;dbname=plugiyqm_pluginmerkezi", 'plugiyqm_qua', 'tl!]cF^5]h_%');
-	//$db = new PDO("mysql:host=localhost;dbname=plugiyqm_pluginmerkezi", 'root', ''); // qua localhost
+	$names=$db->prepare("SET NAMES UTF8");
+	$names->execute();
+	//$db = new PDO("mysql:host=localhost;dbname=plugiyqm_pluginmerkezi", 'root', ''); // localhost
 }
 catch(PDOException $e)
 {
